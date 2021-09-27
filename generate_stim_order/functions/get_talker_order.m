f469f9bb3449f37632a116f018b55b24e05c7e2c
Letitia Ho <letitiayhho@gmail.com>
2021-09-27 17:25:05 -0500

    Add column denoting whether the talkers are the same

--- generate_stim_order/functions/get_talker_order.m
+++ generate_stim_order/functions/get_talker_order.m
@@ -1,20 +1,24 @@
-function [talker1_order, talker2_order] = get_talker_order()
+function [talker1_order, talker2_order, same_order] = get_talker_order()
     talker1 = ["A"; "A"; "B"; "B"; "X"; "X"; "Y"; "Y"];
     talker2 = ["A"; "B"; "A"; "B"; "X"; "Y"; "X"; "Y"];
+    same = [1; 0; 0; 1; 1; 0; 0; 1];
     
     % Training trials
     order = randperm(8);
     talker1_order = talker1(order);
     talker2_order = talker2(order);
+    same_order = same(order);
         
     % Double frequency of same-talker trials for test trials
     talker1 = [talker1; "A"; "B"; "X"; "Y"];
     talker2 = [talker2; "A"; "B"; "X"; "Y"];
+    same = [same; 1; 1; 1; 1];
     
     % Generate order
     for i = 1:20
         order = randperm(12);
         talker1_order = [talker1_order; talker1(order)];
         talker2_order = [talker2_order; talker2(order)];
+        same_order = [same_order; same(order)];
     end
 end
--- generate_stim_order/generate_stim_order.m
+++ generate_stim_order/generate_stim_order.m
@@ -7,24 +7,26 @@ function generate_stim_order(subject_number)
     rng(subject_number)
 
     % Get talker order
-    [talker1, talker2] = get_talker_order();
+    [talker1, talker2, same] = get_talker_order();
 
     % Get vowel order
     [vowel] = get_vowel_order();
 
-    % Block 
+    % Get block number
     block_number = [1, 2, 3, 4, 5, 6];
     block_length = [8, 48, 48, 48, 48, 48];
     block = [];
+    rep = [];
     for i = 1:length(block_length)
         block = [block; repmat(block_number(i), block_length(i), 1)];
+        rep = [rep; (1:block_length(i))'];
     end
 
-    % Subject
+    % Add subject number
     subject = repmat(subject_number, length(block), 1);
 
-    % COLUMNS: subject, block, type, rep, utterance, vowel, istarget, talker
-    stim_order = table(subject, block, vowel, talker1, talker2);
+    % CREATE TABLE
+    stim_order = table(subject, block, rep, vowel, talker1, talker2, same);
 
     % WRITE
     writetable(stim_order, ['output/', num2str(subject_number), '_stim_order.txt'])
