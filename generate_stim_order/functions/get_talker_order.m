f92ed1a1c0655a5403e07bf3d07bb97e6e0b4384
Letitia Ho <letitiayhho@gmail.com>
2021-10-01 09:10:58 -0500

    Generate talker order recursively to prevent too many same or different trials in a row

--- generate_stim_order/functions/get_talker_order.m
+++ generate_stim_order/functions/get_talker_order.m
@@ -21,4 +21,14 @@ function [talker1_order, talker2_order, same_order] = get_talker_order()
         talker2_order = [talker2_order; talker2(order)];
         same_order = [same_order; same(order)];
     end
+    
+    % Make sure there aren't more than 3 same or different trials in a row
+    for j = 1:length(same_order)-8
+        window = j:j+8;
+%         same_order(window)
+        if (sum(same_order(window)) == 0) || (sum(same_order(window)) == 8)
+            % Generate talker order recursively until condition is met
+            [talker1_order, talker2_order, same_order] = get_talker_order();
+        end
+    end
 end
