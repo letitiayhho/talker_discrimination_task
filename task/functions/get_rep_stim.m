35787d7db932066e4029a9527058088ab58aec00
Letitia Ho <letitiayhho@gmail.com>
2021-09-30 22:31:25 -0500

    Construct full stim path using exemplar numbers

--- task/functions/get_rep_stim.m
+++ task/functions/get_rep_stim.m
@@ -4,7 +4,7 @@ function [stim1, stim2, same] = get_rep_stim(stim_order, block, rep)
     stim_order = stim_order(stim_order.rep == rep, :);
         
     % get stim paths
-    stim1 = get_stim_path(stim_order.talker1, stim_order.vowel);
-    stim2 = get_stim_path(stim_order.talker2, stim_order.vowel);
+    stim1 = get_stim_path(stim_order.talker1, stim_order.vowel, stim_order.exemplar1);
+    stim2 = get_stim_path(stim_order.talker2, stim_order.vowel, stim_order.exemplar2);
     same = stim_order.same;
 end
--- task/functions/get_stim_path.m
+++ task/functions/get_stim_path.m
@@ -1,3 +1,4 @@
-function [stim_path] = get_stim_path(talker_order, vowel_order)
-    stim_path = fullfile('stim', talker_order, strcat(vowel_order, '.wav'));
+function [stim_path] = get_stim_path(talker_order, vowel_order, exemplar_order)
+    filename = strcat(vowel_order, num2str(exemplar_order), '.wav');
+    stim_path = fullfile('stim', talker_order, filename);
 end
