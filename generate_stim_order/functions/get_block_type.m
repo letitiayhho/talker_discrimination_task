77d81ced6e454355ee687bfd8292ad2dbe8d47c5
Letitia Ho <letitiayhho@gmail.com>
2021-10-01 21:20:15 -0500

    Clean up repo

--- generate_stim_order/functions/get_block_type.m
+++ generate_stim_order/functions/get_block_type.m
@@ -1,4 +1,4 @@
-function [block_type, n_trials] = get_block_order(BLOCK)
+function [block_type, n_trials] = get_block_type(BLOCK)
     if rand < 0.5
         block_order = ["training", "same", "different", "same", "different"];
     else
