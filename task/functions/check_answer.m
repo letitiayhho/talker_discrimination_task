cacd380a1b9066f4d4fc1a51f38e6b20e39b10c4
Letitia Ho <letitiayhho@gmail.com>
2021-10-01 21:35:07 -0500

    Check answer by comparing expected key and actual resp

--- task/functions/check_answer.m
+++ task/functions/check_answer.m
@@ -1,12 +1,3 @@
-function [correct] = check_answer(same, resp)
-    same = logical(same);
-    if resp == "" % catch empty answers
-        correct = 0;
-    elseif same && resp == 's' % correctly identifies target
-        correct = 1;
-    elseif ~same && resp == 'd' % correctly identifies non-targets
-        correct = 1;
-    else % all other cases
-        correct = 0;
-    end
+function [correct] = check_answer(key, resp)
+    correct = strcmp(key, resp);
 end
