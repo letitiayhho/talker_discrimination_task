682f050fad5826302520d18ac249b6370eae1584
Letitia Ho <letitiayhho@gmail.com>
2021-10-03 12:47:41 -0500

    Remove RTBox

--- task/run.m
+++ task/run.m
@@ -1,26 +1,23 @@
 %%%%%%%%%%%%%%%%%%%%%%%%% UPDATE THIS SECTION BEFORE EACH SUBJECT/TEST
 
 SUBJ_NUM = 0; % numeric
-BLOCK = 2; % numeric
-RTBOX = false; % logical
+BLOCK = 1; % numeric
 
 %%%%%%%%%%%%%%%%%%%%%%%
       
-% PsychDebugWindowConfiguration
+PsychDebugWindowConfiguration
 
 %% Set up
 cd('~/src/talker_discrimination_task/')
 addpath('task/functions')
 addpath('generate_stim_order')
-% addpath('task/USTCRTBox_003')      
-% PsychJavaTrouble(1);
+PsychJavaTrouble(1);
 
 % other constants      
 FS = 44100;
 
 % set up psychtoolbox and RTBox
-% init_RTBox(RTBOX);
-% PTB = init_psychtoolbox(FS);
+PTB = init_psychtoolbox(FS);
 
 % Load stim order
 [STIM, SAME_KEY, N_TRIALS, BLOCK_TYPE] = generate_stim_order(subject_number, BLOCK);
