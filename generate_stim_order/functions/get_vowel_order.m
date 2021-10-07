93d5789ffedb09510e8db2918305814040c162c3
Letitia Ho <letitiayhho@gmail.com>
2021-10-07 13:31:05 -0500

    Rename AH to AA

--- generate_stim_order/functions/get_vowel_order.m
+++ generate_stim_order/functions/get_vowel_order.m
@@ -1,5 +1,5 @@
 function vowel_order = get_vowel_order(n_trials)
-    possible_vowels = ["AH", "EH", "IH", "OO"];
+    possible_vowels = ["AA", "EH", "IH", "OO"];
     vowel_order = [];
     for i = 1:n_trials/length(possible_vowels)
         vowel_order = [vowel_order; datasample(possible_vowels, 4, 'Replace', false)'];
