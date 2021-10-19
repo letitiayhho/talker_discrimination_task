c3be23a0d6a22b8c468ce59e2972b73db25199d3
Letitia Ho <letitiayhho@gmail.com>
2021-10-19 16:50:45 -0500

    Update check_repeats to account for shorter training blocks

--- generate_stim_order/functions/get_vowel_order.m
+++ generate_stim_order/functions/get_vowel_order.m
@@ -1,4 +1,4 @@
-function [vowel1, vowel2] = get_vowel_order(n_trials)
+function [vowel1, vowel2] = get_vowel_order(block, n_trials)
     possible_vowels = ["AA", "EH", "IH", "OO"];
 
     while true
@@ -9,20 +9,36 @@ function [vowel1, vowel2] = get_vowel_order(n_trials)
         end
         vowel1 = vowel_order(:, 1);
         vowel2 = vowel_order(:, 2);
-    
-        % Make sure count of each vowel is min 8 max 12
-        if check_counts(vowel1) && check_counts(vowel2)
-            break
+        
+        % Make sure count of each vowel is min 1 max 2 for training blocks
+        if block == 1 &&...
+            check_counts(vowel1, 1, 3) &&...
+            check_counts(vowel2, 1, 3)
+            return
         end
-            
+    
+        % Make sure count of each vowel is min 8 max 12 for test blocks
+        if block ~= 1 &&...
+            check_counts(vowel1, 8, 12) &&...
+            check_counts(vowel2, 8, 12)
+            return
+        end  
     end
     
-    function break_loop = check_counts(seq)
+    function break_loop = check_counts(seq, min, max)        
         counts = groupcounts(seq);
-        if sum(counts <= 8) || sum(counts >= 12)
+        
+        % Make sure there is one sample of each vowel
+        if length(counts) < 4
             break_loop = false;
-        else
+            return
+        end
+
+        % Make sure each vowel appears at least a certain number of times
+        if sum(counts < min) == 0 || sum(counts > max) == 0
             break_loop = true;
+        else
+            break_loop = false;
         end
     end
 end
